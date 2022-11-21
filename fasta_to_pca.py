9449d3e5e35e4c413379dc8f7e208337a6a43c50
fmolls <fabian.molls@uni-bielefeld.de>
2022-11-21 16:56:39 +0100

    Optional parameter -p for position marking availabel. If used, additional PCA Plot with coordinates calculated only with kmeres containing marked position will be generated

--- fasta_to_pca.py
+++ fasta_to_pca.py
@@ -10,10 +10,12 @@ yco = []
 fastaData = {}
 matrixpca = [[]]
 fasta_names = []
+pm_pca = {}
 parser = argparse.ArgumentParser("This programm calculates the k-mers of choosen Fasta Data and plots a 2d pca. FASTA Data has to be saved to fasta directory and number of k-mers has to be given as command argument -k.")
 parser.add_argument("-k","--k_mers", help="number of k-mers", type=int, choices=[1,2,3,4,5,6,7,8,9])
 parser.add_argument("-f","--fasta", nargs='+', help="fasta data for pca")
 parser.add_argument("-o","--output", help="if set, plot will be saved to png with parsed name, else plot will be")
+parser.add_argument("-p", "--position", help='Additional parameter for position marking, if set a additional pca will be generated only with seqences containing set Marker')
 args = parser.parse_args()
     
 def k_mer_calc(k):
@@ -28,7 +30,12 @@ def k_mer_calc(k):
 	result = {}
 	i = 0
 	for Value in fastaData.values():
+		kmer_dict_intermediate_pm = kmer_dict.copy()
 		kmer_dict_intermediate = kmer_dict.copy()
+		try:
+			p = int(args.position)
+		except TypeError:
+			print("no position marker set")
 		for record in Value:
 			start = 0
 			end = start + k
@@ -36,7 +43,18 @@ def k_mer_calc(k):
 				kmer_dict_intermediate[record.seq[start:end]] = kmer_dict_intermediate.get(record.seq[start:end]) + 1
 				start = start + 1
 				end = end + 1
+			try:
+					start = p - k
+					end = k
+					while start < (len(record.seq) - k + 1) and start < p:
+						kmer_dict_intermediate_pm[record.seq[start:end]] = kmer_dict_intermediate_pm.get(
+							record.seq[start:end]) + 1
+						start = start + 1
+						end = end + 1
+			except UnboundLocalError:
+				pass
 		result[fasta_names[i]] = kmer_dict_intermediate
+		pm_pca[fasta_names[i]] = kmer_dict_intermediate_pm
 		i = i + 1
 	return result
 
@@ -58,11 +76,38 @@ def plot_pca(matrixpca):
 		plt.annotate(txt, (xco[i],yco[i]))
 	try:
 		plt.savefig(args.output)
-		sys.exit()
 	except NameError:
 		plt.show()
-	else:   	
+	try:
+		p = int(args.position)
+		xco_pm = []
+		yco_pm = []
+		intermediate = []
+		i = 0
+		for l in list(pm_pca):
+			intermediate.append(list(pm_pca[fasta_names[i]].values()))
+			i = i + 1
+		columnmean_pm, coordinates_pm, components_pm, eigenvalues_pm = pca(intermediate)
+		i = 0
+		for filename in args.fasta:
+			xco_pm.append(coordinates_pm[i][0])
+			yco_pm.append(coordinates_pm[i][1])
+			i = i + 1
+		plt.scatter(xco_pm, yco_pm)
+		i = 0
+		for i, txt in enumerate(fasta_names):
+			plt.annotate(txt, (xco_pm[i], yco_pm[i]))
+		try:
+			plt.savefig(args.output + "_with_position_marker")
+			sys.exit()
+		except NameError:
+			plt.show()
+	except UnboundLocalError:
+		print("position marker position marker not set")
 		sys.exit()
+	else:
+		sys.exit()
+
     
 def main():
 	try:
@@ -77,7 +122,7 @@ def main():
 				fasta_names.append(filename)
 			plot_pca(k_mer_calc(k))
 		except TypeError:
-			print("please input fasta data for computing pca")
+			pass
 			sys.exit()
 		else:
 			sys.exit()	
